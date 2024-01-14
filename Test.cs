15cb823a22169ed3516a28939ba0bf4111587dd3
Zme-gun <156582017+Zme-gun@users.noreply.github.com>
2024-01-14 14:56:52 +0300

    Create Test.cs

---
+++ Test.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int X = 1000;
+        print("HelloWorld");
+        print(2+2);
+        if (X>0){
+print (X-7);
+        } else {
+print (X-7);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
